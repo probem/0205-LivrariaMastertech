c3109347acc7b4f815f81760da2c2d8fe62c3fd8
Gabriel Roger <gabriel.nroger@gmail.com>
2020-01-17 13:59:46 -0300

    criar classe autor com metodos

---
+++ src/SistemaLivraria/Autor.java
@@ -0,0 +1,79 @@
+package SistemaLivraria;
+
+public class Autor {
+    private String nome;
+    private String sobrenome;
+    private String generoLiterario;
+    private char sexo;
+    private String pais;
+    private String cidade;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getGeneroLiterario() {
+        return generoLiterario;
+    }
+
+    public void setGeneroLiterario(String generoLiterario) {
+        this.generoLiterario = generoLiterario;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String listarDados() {
+        return "Autor{" +
+                "nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", generoLiterario='" + generoLiterario + '\'' +
+                ", sexo=" + sexo +
+                ", pais='" + pais + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
